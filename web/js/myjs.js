eda4402d34b642f3cb3e776cf9f96b59b83c64a1
carlos <carlosn6@gmail.com>
2019-02-20 12:15:54 +0100

    Iniciado proyecto, incluye mapeos, archivos css, etc

---
+++ web/js/myjs.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    console.log('ready');
+    init();
+});
+function init() {
+    loadApuestas();
+    apostar();
+}
+
+function apostar(){
+      $('#modalApuesta').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+        var idpartido = button.data('id');
+        var nombrepartido = button.data('whatever');
+        var modal = $(this);
+	modal.find('#partidomodal').text(nombrepartido);
+        modal.find('#idoculto').val(idpartido);
+        
+    });
+}
+
+function loadApuestas() {
+    $('#modalInfo').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+        var idpartido = button.data('id');
+        $.ajax({
+            type: "GET",
+            url: "Controller?op=apuestas&partido=" + idpartido,
+            success: function (info) {
+                $("#modalinfoapuestas").html(info);
+            }
+        });
+    });
+}
+
+
+
+
+
+
